51df9ba02272977169947f2a7ecc85b188a054b8
ChristopherM <89361839+Rinsey@users.noreply.github.com>
2023-04-07 19:30:09 -0700

    Update separarCadenaComas.ino

--- separarCadenaComas.ino
+++ separarCadenaComas.ino
@@ -1,13 +1,18 @@
 
+//Ejemplo xd
+
 float valores[8]; //Aqui se guardan los valores que vengan de la raspberry el orden esta por decidir Distancia,Angulo,Circulo,Lock
 int longitud = 8; //Aqui se pone lo que mide el arreglo
 String cadena = "asd,23.4,123.2,123.33,23.0,hola,2323.978,434.0"; //Cadena con 8 valores para extraer
 
 void setup() {
+  
   Serial.begin(115200); 
+  
 }
 
 void loop() {
+  
   separarCadena(cadena); //Separamos la cadena de ejemplo
 
   Serial.println("-=-=-=-=-=-=-=-=-=-=-");
@@ -15,25 +20,27 @@ void loop() {
     Serial.println(valores[x]); //Mostramos los valores guardados.
   }
   delay(5000);
+  
 }
 
 void separarCadena(String cadena) {
   
   int inicio = 0; //Importante para encontrar cada coma en la sentencia
-  int fin = 0; // Para marcar la siguiente coma
-  int i = 0; // para insertar el dato ya convertido en su posicion correspondiente
+  int finall = 0; // Para marcar la siguiente coma
+  int contador = 0; // para insertar el dato ya convertido en su posicion correspondiente es contador
 
-  while (fin >= 0 && i < longitud) { //Se puede cambiar por un for 
-    fin = cadena.indexOf(',', inicio); // Encontramos la posicion de una coma, desde el punto de inicio el cual cambia dependiendo de las comas encontradas
-    if (fin == -1) { //Esto es en caso de que ya no se encuentre ninguna coma
-      fin = cadena.length(); //Si no se encuentra ninguna coma, el fin para el substring seria el final de la cadena o sea su longitud de caracteres
+  while (finall >= 0 && contador < longitud) { //Se puede cambiar por un for 
+    finall = cadena.indexOf(',', inicio); // Encontramos la posicion de una coma, desde el punto de inicio el cual cambia dependiendo de las comas encontradas
+    if (finall == -1) { //Esto es en caso de que ya no se encuentre ninguna coma
+      finall = cadena.length(); //Si no se encuentra ninguna coma, el fin para el substring seria el final de la cadena o sea su longitud de caracteres
     }
-    String valorStr = cadena.substring(inicio, fin); //Se extrae el valor entre dos comas.
-    inicio = fin + 1; //se establece un nuevo inicio para empezar a buscar las comas y para extraer valores
+    
+    String valorStr = cadena.substring(inicio, finall); //Se extrae el valor entre dos comas.
+    inicio = finall + 1; //se establece un nuevo inicio para empezar a buscar las comas y para extraer valores
     float valor = valorStr.toFloat(); //Se convierte a float, aqui pasa algo chido si no se puede regresa un nan el cual se puede comprobar 
     if (!isnan(valor)) { //Aqui comprobamos si el valor se pudo convertir, en caso de que no pues se ignora y se añade un 0
-      valores[i] = valor; //Si el valor si es valido se almacena
-      i++;// se recorre la posicion en la que se guarda el valor.
+      valores[contador] = valor; //Si el valor si es valido se almacena
+      contador++;// se recorre la posicion en la que se guarda el valor.
     }
   }
   
